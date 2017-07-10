c8ee0a8fb01a14d6b629b2bec27a42bc3753ee42
Trzaska <Niklas.Trzaska@uni-muenster.de>
2017-07-10 17:49:25 +0200

    Put sound where it belongs to be.

--- js/soundOnBorderCrossing.js
+++
@@ -1,9 +0,0 @@
-// const borderSound = document.createElement('audio');
-// console.log(borderSound)
-// borderSound.id="borderAudio";
-// borderSound.src = "../sound/border.mp3";
-
-// <audio id="borderAudio" src="../sound/border.mp3"></audio>
-function playBorderSound() {
-$('#borderAudio')[0].play();
-};
