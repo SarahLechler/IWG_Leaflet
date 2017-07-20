25977e7c1884cf86138b6c2bda3d2b2dff0616c0
SarahLechler <lechler.sarah@gmail.com>
2017-07-20 22:19:43 +0200

    white noise added

--- sound/initiate2Sounds.js
+++ sound/initiate2Sounds.js
@@ -6,7 +6,7 @@ var dimension2 = audiocontext.createStereoPanner();
 
 
 function create2Sounds(pianoKey1, pianoKey2, startTime, endTime, direction1, direction2) { // startTime is always audiocontext.currentTime and endTime audiocontext.currentTime + how long it should be played
-    if (sound1 === null) {
+    if (sound1 === null && sound2 === null) {
         createWhiteNoise(startTime, endTime)
     } else {
         var g = audiocontext.createGain();
@@ -21,31 +21,27 @@ function create2Sounds(pianoKey1, pianoKey2, startTime, endTime, direction1, dir
         console.log(sound1);
         sound1.connect(g);
         createDimension(direction1);
-    }
-    ;
-if (sound2 === null){
-        createWhiteNoise(startTime, endTime)
-} else {
-    var sound2 = audiocontext.createOscillator();
-    sound2.channelCountMode = "clamped-max";
-    sound2.channelCount = "2";
-    sound2.channelInterpretation = "speakers";
-    sound2.type = "sine"; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
-    sound2.connect(audiocontext.destination);
-    sound2.frequency.value = calculateFrequency(pianoKey2);
-    console.log(sound2);
-    sound2.connect(g);
-    createDimension(direction2);
-    }
-    
-    sound1.connect(dimension1);
-    sound2.connect(dimension2);
-    dimension1.connect(audiocontext.destination);
-    dimension2.connect(audiocontext.destination);
+        var sound2 = audiocontext.createOscillator();
+        sound2.channelCountMode = "clamped-max";
+        sound2.channelCount = "2";
+        sound2.channelInterpretation = "speakers";
+        sound2.type = "sine"; // sine wave — other values are 'square', 'sawtooth', 'triangle' and 'custom'
+        sound2.connect(audiocontext.destination);
+        sound2.frequency.value = calculateFrequency(pianoKey2);
+        console.log(sound2);
+        sound2.connect(g);
+        createDimension(direction2);
 
-    sound1.start(startTime);
-    sound2.start(startTime);
-    sound1.stop(endTime);
-    sound2.stop(endTime);
+        sound1.connect(dimension1);
+        sound2.connect(dimension2);
+        dimension1.connect(audiocontext.destination);
+        dimension2.connect(audiocontext.destination);
+
+        sound1.start(startTime);
+        sound2.start(startTime);
+        sound1.stop(endTime);
+        sound2.stop(endTime);
+    }
+
 }
 ;
